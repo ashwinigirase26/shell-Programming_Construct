1dfc17e7215c22921c96d0eea02474f04f426725
“Enter <ashwinigirase26@gmail.com>
2020-07-16 23:51:06 +0530

    ADDED:takes the user input and prints the factorial of that number.

---
+++ Repetation/forloop_Problems/factorialNumber.sh
@@ -0,0 +1,10 @@
+#Write a program that computes a factorial of a number taken as input.
+#!/bin/bash -x
+echo "enter the number"
+read num;
+fact=1;
+for((i=1;i<=$num;i++))
+do
+fact=$(( $fact * $i ))
+done
+echo "$num of factorial is =$fact";
